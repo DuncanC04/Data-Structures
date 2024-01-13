6b3a870750b9d6f43990203f21800856feb9232a
DuncanC04 <33468504+DuncanC04@users.noreply.github.com>
2024-01-12 21:30:14 -0500

    Update StackLL.java

--- StackLL.java
+++ StackLL.java
@@ -8,40 +8,36 @@ public class StackLL{
 	private int n;
 	
 	public StackLL(){
-    s = null;
-    n = 0;
-  }
+    		s = null;
+    		n = 0;
+	}
   
-  public Node createEmptyStack(){ //returns a newly created empty stack
-    return s;
-  }
+  	public Node top(){ //returns the last node of S
+    		return s;
+  	}
   
-  public Node top(){ //returns the last node of S
-    return s;
-  }
+  	public Node pop(){ //returns and removes the last node of S
+    		n--;
+    		Node temp = s;
+    		s = s.getNext();
+    		return temp;
+  	}
   
-  public Node pop(){ //returns and removes the last node of S
-    n--;
-    Node temp = s;
-    s = s.getNext();
-    return temp;
-  }
+  	public void push(Node x){ //adds x in front of head node, pushes x on top
+    		x.setNext(s);
+    		s = x;
+    		n++;
+  	}
   
-  public void push(Node x){ //adds x in front of head node, pushes x on top
-    x.setNext(s);
-    s = x;
-    n++;
-  }
+  	public boolean isEmpty(){ //returns true if S is empty and false if it is not
+    		return n==0;
+  	}
   
-  public boolean isEmpty(){ //returns true if S is empty and false if it is not
-    return n==0;
-  }
-  
-  public void printStack() {// printStack method for StackLL
-    Node temp = s;
-    while (temp != null) {
-      System.out.println(temp.getKey());
-      temp = temp.getNext();
-    }
-  }
+  	public void printStack() {// printStack method for StackLL
+    		Node temp = s;
+    		while (temp != null) {
+      			System.out.println(temp.getKey());
+      			temp = temp.getNext();
+    		}
+  	}
 }
