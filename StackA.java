91825a99e5c67b0d4ce004425f1e9a7e8e9ad9de
DuncanC04 <33468504+DuncanC04@users.noreply.github.com>
2024-01-12 21:28:55 -0500

    Update StackA.java

--- StackA.java
+++ StackA.java
@@ -9,36 +9,36 @@ public class StackA{
 	private int n;
 	
 	public StackA(){
-    n = 0;
-  }
+    		n = 0;
+	}
   
-  public Node[] createEmptyStack(){ //returns a newly created empty stack
-    return s;
-  }
+  	public Node[] createEmptyStack(){ //returns a newly created empty stack
+    		return s;
+  	}
   
-  public Node top(){ //returns the last node of S
-    return s[n-1];
-  }
+  	public Node top(){ //returns the last node of S
+    		return s[n-1];
+  	}
   
-  public Node pop(){ //returns and removes the last node of S
-    n--;
-    return s[n];
-  }
+  	public Node pop(){ //returns and removes the last node of S
+    		n--;
+    		return s[n];
+  	}
   
-  public Node[] push(Node x){ //returns a S with x added as the last element
-    s[n] = x;
-    n++;
-    return s;
-  }
+  	public Node[] push(Node x){ //returns a S with x added as the last element
+    		s[n] = x;
+    		n++;
+    		return s;
+  	}
   
-  public boolean isEmpty(){ //returns true if S is empty and false if it is not
-    return n==0;
-  }
+  	public boolean isEmpty(){ //returns true if S is empty and false if it is not
+    		return n==0;
+  	}
   
-  public void printStack() {// printStack method for StackA
-    System.out.println(n);
-    for(int i = n-1; i >= 0; i--) {
-      System.out.println(s[i].getKey());
-    }
-  }
+  	public void printStack() {// printStack method for StackA
+    		System.out.println(n);
+    		for(int i = n-1; i >= 0; i--) {
+      			System.out.println(s[i].getKey());
+    		}
+  	}
 }
